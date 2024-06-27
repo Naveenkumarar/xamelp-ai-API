773c2181a91349ded35b51d76ba416f6b5772930
Naveenkumarar <rajarajannaveenkumar@gmail.com>
2024-06-26 21:24:28 -0400

    change media url

--- pdf_admin/urls.py
+++ pdf_admin/urls.py
@@ -8,7 +8,3 @@ urlpatterns = [
     path('getmcq', views.get_mcq, name='get_mcq'),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
