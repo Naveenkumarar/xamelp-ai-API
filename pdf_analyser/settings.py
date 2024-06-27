4b7e878bd2c04fe36e45f23a2b650dd3341968d5
Ubuntu <ubuntu@ip-172-31-7-85.us-east-2.compute.internal>
2024-06-27 00:49:05 +0000

    aws setting

--- pdf_analyser/settings.py
+++ pdf_analyser/settings.py
@@ -95,7 +95,7 @@ if debug == 1:
     }
     
 else:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    #SECRET_KEY = os.environ.get("SECRET_KEY")
     DEBUG = False
     # db_from_env = dj_database_url.config(conn_max_age=600)
     # DATABASES = {'default':db_from_env}
